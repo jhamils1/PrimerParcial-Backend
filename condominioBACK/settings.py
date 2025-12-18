2f465df00008fc42f7d32b6ba67f33d207e1e065
scrumjhamil <scrumjhamil@gmail.com>
2025-12-17 22:37:08 -0400

    cambios locales

--- condominioBACK/settings.py
+++ condominioBACK/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*','condominio-backend-production.up.railway.app', '192.168.0.13']
+ALLOWED_HOSTS = ['*','condominio-backend-production.up.railway.app', '192.168.0.11']
 
 
 # Application definition
